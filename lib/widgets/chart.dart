c7b2db104ba5a29bfaa354d367bc79dbf376a115
Fernando Christyanto <christyantofernando@gmail.com>
2019-12-22 23:52:06 +0700

    initial commit

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,58 @@
+import 'package:expense_tracker/models/transaction.dart';
+import 'package:expense_tracker/widgets/chart_bar.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      // find out which weekday to generate
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      double totalSum = 0.0;
+      for (Transaction tx in recentTransactions) {
+        if (tx.date.day == weekDay.day &&
+            tx.date.month == weekDay.month &&
+            tx.date.year == weekDay.year) totalSum += tx.amount;
+      }
+
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
+    }).reversed.toList();
+  }
+
+  double get totalspending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: groupedTransactionValues.map((data) {
+              return Flexible(
+                fit: FlexFit.tight,
+                child: ChartBar(
+                    data['day'],
+                    data['amount'],
+                    totalspending == 0.0
+                        ? 0.0
+                        : (data['amount'] as double) / totalspending),
+              );
+            }).toList()),
+      ),
+    );
+  }
+}
